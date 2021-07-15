836bd3e9a3cb57bc7a8f5f683235a6f600142ab1
Provit Chordia <chordiaprovit@gmail.com>
2021-07-15 00:03:40 -0400

    added new files

--- app/flaskApp.py
+++ app/flaskApp.py
@@ -1,34 +1,46 @@
 import pickle
-from flask import Flask, request
+from flask import Flask, request, render_template
 import pandas
 from pandas import DataFrame
 from app.format import *
 
 app = Flask(__name__)
 
-model = pickle.load(open('/Users/provitchordia/PycharmProjects/predictProject/app/lr_model.sav', 'rb'))
+model = pickle.load(open('app/lr_model.sav', 'rb'))
 
-@app.route('/predict',methods=['GET'])
+@app.route('/predict',methods=['POST','GET'])
 def my_app():
-    dict = request.get_json()
-    if dict is not None :
-        vals =DataFrame(list(dict.values())).T
-        data = formatData(vals)
-        score = model.predict(data)
+    Gender =  request.form.get("Gender")
+    Married =  request.form.get("Married")
+    Dependents =  request.form.get("Dependents")
+    Education =  request.form.get("Education")
+    Self_Employed =  request.form.get("Self_Employed")
+    ApplicantIncome = request.form.get("ApplicantIncome")
+    CoapplicantIncome = request.form.get("CoapplicantIncome")
+    LoanAmount = request.form.get("LoanAmount")
+    Loan_Amount_Term = request.form.get("Loan_Amount_Term")
+    Credit_History = request.form.get("Credit_History")
+    Property_Area =  request.form.get("Property_Area")
 
-        if score[0] == 1:
-            out = 'score for model = {}'.format(score) + " Loan Approved"
-        else:
-            out = 'score for model = {}'.format(score) + " Loan Denied"
+    vals = [Gender,Married,Dependents,Education,Self_Employed,ApplicantIncome,CoapplicantIncome,LoanAmount,Loan_Amount_Term,Credit_History,Property_Area]
+
+    data = formatData(DataFrame(vals).T)
+    print(data.to_numpy())
+    score = model.predict(data)
+    # dict = request.get_json()
+    # if dict is not None :
+    #     vals =DataFrame(list(dict.values())).T
+    if score[0] == 1:
+       out = 'score for model = {}'.format(score) + " Loan Approved"
     else:
-        out = "pass"
-    print(out)
-    return out
+       out = 'score for model = {}'.format(score) + " Loan Denied"
 
-# @app.route('/')
-# def my_app_test():
-#     print("hello world")
-#     return {'hello': 'world'}
+    print(out)
+    return render_template("index.html", out=out)
+
+@app.route('/')
+def my_app_test():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- app/format.py
+++ app/format.py
@@ -4,7 +4,6 @@ np.seterr(divide='ignore')
 
 
 def formatData(data):
-    print(data)
     '''convert male and female to 0 and 1'''
     data[0] = data[0].map({'Male': 0, 'Female': 1})
 
@@ -21,11 +20,12 @@ def formatData(data):
     ''' covert self employed to Boolean and assuming blanks are False'''
     data[4] = data[4].map(dict(Yes=True, No=False))
 
+    data[6] = data[6].astype(np.float64)
     '''assuming that loan term is 360 if not filled in'''
     data[7] = data[7].astype(np.float64)
 
     '''assuming that loan term is 360 if not filled in'''
-    data[8] = 0
+    data[8] =  0
 
     ''' converting Property from string to int'''
     data[10] = data[10].map({'Urban': 1, 'Semiurban': 2, 'Rural': 3})
